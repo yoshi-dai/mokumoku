f86d471a2f15afbdd0da1e78d881f9c703290b3c
yoshida daiki <yoshida.daiki.51@gmail.com>
2023-04-08 02:45:23 +0900

    Userにgenderカラムの追加とUserモデルにenum設定まで。

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,8 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
 
+  enum gender: { male: 0, female: 1, other: 2 }
+  
   scope :allowing_created_event_notification,
         -> { joins(:notification_timings).merge(NotificationTiming.created_event) }
   scope :allowing_commented_to_event_notification,
